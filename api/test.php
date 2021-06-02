c8a33ee3213c90dbf8e0a3e66aa9893bd62651a2
rushikaliye92 <60660435+rushikaliye92@users.noreply.github.com>
2021-06-02 20:04:01 +0530

    Update test.php

--- api/test.php
+++ api/test.php
@@ -6,7 +6,7 @@ $password = "b8*db8&K(m|OrF3(";
 $db = "id16918211_php_auth_api";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
