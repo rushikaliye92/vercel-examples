bba3ee3fc67375faac4b252deff7e7fc2a851c05
rushikaliye92 <60660435+rushikaliye92@users.noreply.github.com>
2021-06-02 20:03:13 +0530

    Create test.php

---
+++ api/test.php
@@ -0,0 +1,17 @@
+<?php 
+
+$servername = "localhost";
+$username = "id16918211_recipe_user";
+$password = "b8*db8&K(m|OrF3(";
+$db = "id16918211_php_auth_api";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+?>
